11c5457394a140aef7c0ba240902970f9e20a92d
Shib235 <100515812+Shib235@users.noreply.github.com>
2022-03-31 09:49:38 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-mkdir -p ~/.streamlit/
+mkdir -p ~/.pygame/
 echo "\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
-" > ~/.streamlit/config.toml
+" > ~/.pygame/config.toml
